400ef7e38067edd3558bb5511fc43b1956b8b907
kosaksongul <kosaksongul@gmail.com>
2024-02-19 23:13:35 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degiskenler();
+
+            Vatandas vatandas1 = new Vatandas();
+
+            SelamVer();
+            SelamVer();
+            SelamVer();
+
+            int sonuc = Topla();
+
+            Console.ReadKey();
+        }
+
+        //methot 
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba");
+        }
+        static int Topla()
+        {
+            return 5;
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Selam";
+            double tutar = 10000; //db den gelir 
+            int sayi1 = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Engin";
+            string soyad = "Demiroğ";
+            int dogumYili = 1985;
+            long tcNo = 12345678910;
+
+            Console.WriteLine(tutar * 1.10);
+            Console.WriteLine(tutar * 1.10);
+        }
+    }
+    //pascal casing
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+
+}
